39edcd8e2cf001fb5ef87f87957bb104bd45522a
JonathanL83 <jonathanlynch@outlook.ie>
2024-07-12 18:51:09 +0000

    Final

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-#include "mbed.h"
+#include <studio.h>
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+int main() {
+    // Initialize variables
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d, e;
 
-    }
+    // Add values of a and b
+    d = a + b;
+    printf("Value of d (a + b): %d\n", d);
+
+    // Subtract variable c from d
+    e = d - c;
+    printf("Value of e (d - c): %d\n", e);
+
+    return 0;
 }
+
 
